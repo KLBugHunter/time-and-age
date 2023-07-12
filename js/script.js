3d6d1b225523f236e94ba2b54ea3f5b9e7c14681
Kamilly Lopes <kamillyvlopesf@gmail.com>
2023-07-12 17:19:06 -0300

    add pg

--- js/script.js
+++ js/script.js
@@ -6,15 +6,15 @@ function carregar() {
     msg.innerHTML = `Now it's ${hora} o'clock.`
     if(hora >= 0 && hora < 12){
         //bom dia
-        img.src = 'imagens/manha.png'
+        img.src = './imgs/manha.png'
         document.body.style.backgroundColor = '#eabf60'
     } else if (hora >= 12 && hora <18){
         //boa tarde
-        img.src = 'imagens/tarde.png'
+        img.src = './imgs/tarde.png'
         document.body.style.backgroundColor = '#608385'
     } else {
         //boa noite
-        img.src = 'imagens/noite.png'
+        img.src = './imgs/noite.png'
         document.body.style.backgroundColor = '#02314b'
     }
 }
